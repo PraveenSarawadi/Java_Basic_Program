ab9d703f0847258f5159a86cac2ea0bb8af0dc2c
PraveenSarawadi <praveensarawadi28@gmail.com>
2025-02-12 22:12:02 +0530

    ArrayList implementation

--- Java_Basic_Project/src/com/practice/Array/PracticePrograms/ArrayPrograms.java
+++ Java_Basic_Project/src/com/practice/Array/PracticePrograms/ArrayPrograms.java
@@ -47,14 +47,14 @@ public class ArrayPrograms {
 //        Example Output:
 //        Largest: 8
 //        Smallest: -5
-        Scanner sc =new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter no.of elements of an array");
-        int arraySize=sc.nextInt();
-        int[] inputArr= new int[arraySize];
-        for (int i=0; i<inputArr.length;i++){
-            inputArr[i]=sc.nextInt();
+        int arraySize = sc.nextInt();
+        int[] inputArr = new int[arraySize];
+        for (int i = 0; i < inputArr.length; i++) {
+            inputArr[i] = sc.nextInt();
         }
-        Day1Programs que1=new Day1Programs();
+        Day1Programs que1 = new Day1Programs();
 //        System.out.println("smallest and Largest number from the array of elements is :"+ Arrays.toString(que1.largestAndSmallestNumber(inputArr)));
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -64,8 +64,98 @@ public class ArrayPrograms {
 //        Example Input:
 //        arr = [1, 2, 4, 6, 3, 7, 8]
 //        n = 8
-        Day1Programs que2=new Day1Programs();
-        System.out.println("Array: "+Arrays.toString(inputArr));
-        System.out.println("missing no from the array is :"+que2.missingNumber(inputArr));
+        Day1Programs que2 = new Day1Programs();
+//        System.out.println("Array: " + Arrays.toString(inputArr));
+//        System.out.println("missing no from the array is :" + que2.missingNumber(inputArr));
+
+
+//    3. Reverse an Array
+//    Problem: Given an array, reverse it without using any extra array or data structure.
+//    Input: [1, 2, 3, 4, 5]
+//    Output: [5, 4, 3, 2, 1]
+//    4. Find the Second Largest Element in an Array
+//    Problem: Given an array of integers, find the second largest element.
+//            Input: [12, 35, 1, 10, 34, 1]
+//    Output: 34
+//    5. Check if an Array is Sorted
+//    Problem: Given an array of integers, check if the array is sorted in non-decreasing order.
+//
+//    Input: [1, 2, 3, 4, 5]
+//
+//    Output: True
+//
+//    Input: [5, 3, 4, 1, 2]
+//
+//    Output: False
+//
+//    6. Move Zeros to End of Array
+//    Problem: Given an array, move all the zeros to the end of the array while maintaining the relative order of the non-zero elements.
+//    Input: [0, 1, 9, 0, 3, 12]
+//    Output: [1, 9, 3, 12, 0, 0]
+//    7. Find the Majority Element
+//    Problem: Given an array of size n, find the element that appears more than n/2 times. The majority element always exists.
+//            Input: [3, 3, 4, 2, 4, 4, 2, 4, 4]
+//    Output: 4
+//    8. Union and Intersection of Two Arrays
+//    Problem: Given two arrays, find the union and intersection of the two arrays.
+//            Input: arr1 = [1, 2, 3, 4, 5], arr2 = [4, 5, 6, 7, 8]
+//    Output:
+//    Union: [1, 2, 3, 4, 5, 6, 7, 8]
+//    Intersection: [4, 5]
+//    9. Rotate an Array by K Positions
+//    Problem: Given an array and a number k, rotate the array to the right by k steps.
+//            Input: [1, 2, 3, 4, 5, 6], k = 2
+//    Output: [5, 6, 1, 2, 3, 4]
+//    10. Find the Duplicate in an Array
+//    Problem: Given an array of n+1 integers, where each integer is between 1 and n, find the duplicate number.
+//            Input: [1, 3, 4, 2, 2]
+//    Output: 2
+//    11. Check if Array Contains Duplicate
+//    Problem: Given an array of integers, check if there are any duplicates in the array.
+//            Input: [1, 2, 3, 1]
+//    Output: True
+//    12. Find the Pair with Given Sum
+//    Problem: Given an array of integers and a sum, find two numbers in the array that add up to the sum.
+//            Input: arr = [1, 2, 4, 3, 5], sum = 6
+//    Output: Pair found: (1, 5) or (2, 4)
+//    13. Largest Sum Contiguous Subarray (Kadane's Algorithm)
+//            Problem: Given an array of integers, find the largest sum of a contiguous subarray.
+//            Input: arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+//                                     Output: 6 (Subarray [4, -1, 2, 1])
+//    14. Count Inversions in an Array
+//    Problem: Given an array, count the number of inversions. An inversion is a pair of indices (i, j) such that arr[i] > arr[j] and i < j.
+//    Input: [2, 4, 1, 3, 5]
+//    Output: 3 (Inversions: (2, 1), (4, 1), (4, 3))
+//    15. Find the Longest Consecutive Sequence
+//    Problem: Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
+//    Input: [100, 4, 200, 1, 3, 2]
+//    Output: 4 (The sequence is [1, 2, 3, 4])
+//    16. find the third highest product of two elements from an array
+
+        System.out.println("Enter no.of elements of an array");
+        int arraySize1 = sc.nextInt();
+        Integer[] inputArr1 = new Integer[arraySize1];
+        for (int i = 0; i < inputArr1.length; i++) {
+            inputArr1[i] = sc.nextInt();
+        }
+        Arrays.sort(inputArr1,Collections.reverseOrder());
+        System.out.println(Arrays.toString(inputArr1));
+        int curr, prev,ind1, ind2;
+//        1,2,3,4,5,6,7
+        int i=1;
+            prev=inputArr1[i-1]*inputArr1[i+1];
+            curr=inputArr1[i]*inputArr1[i+1];
+            if(prev<curr){
+                ind1=i-1;
+                ind2=i+1;
+
+            }
+            else {
+                ind1=i;
+                ind2=i+1;
+
+        }
+        System.out.println("two elements which give third highest product of an array: " +inputArr1[ind1]+" | "+inputArr1[ind2]);
+
     }
 }
--- Java_Basic_Project/src/com/practice/Array/SingleDimensionalArray.java
+++ Java_Basic_Project/src/com/practice/Array/SingleDimensionalArray.java
@@ -9,7 +9,7 @@ public class SingleDimensionalArray {
     public SingleDimensionalArray(int sizeOfArray){
         defaultArr=new int[sizeOfArray];
         for (int i=0;i<defaultArr.length;i++){
-            defaultArr[i]=Integer.MIN_VALUE;
+            defaultArr[i]=0;
         }
     }
 // Inserting element at the index
@@ -33,7 +33,41 @@ public class SingleDimensionalArray {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-  }public static void main(String[] args) {
+  }
+//  check whether the element present in the array
+  public boolean containsElement(int searchElement) {
+/* time complexity - O(n)
+      try {
+          for (int element : defaultArr) {
+              if (element == searchElement)
+                  return true;
+          }
+          return false;
+      } catch (Exception e) {
+          throw new RuntimeException(e);
+      }
+ */
+      try {
+          for (int i=0;i<= defaultArr.length/2;i++) {
+              if (defaultArr[i] == searchElement||defaultArr[defaultArr.length-(i+1)] == searchElement)
+                  return true;
+          }
+          return false;
+      } catch (Exception e) {
+          throw new RuntimeException(e);
+      }
+
+  }
+// delete element from te input index of an array
+  public String deleteElement(int index){
+        try{
+            defaultArr[index]=0;
+            return "element deleted";
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new RuntimeException(e);
+        }
+  }
+  public static void main(String[] args) {
 //        array declaration
         Scanner sc= new Scanner(System.in);
         int size=sc.nextInt();
@@ -44,7 +78,16 @@ public class SingleDimensionalArray {
         obj1.insert(3,23);
 
         System.out.println(Arrays.toString(obj1.defaultArr));
-        System.out.println(obj1.getValue(3));
-        obj1.traverseArray();
+//        System.out.println(obj1.getValue(3));
+//        obj1.traverseArray();
+      System.out.println("enter search element in the array");
+      int searchElement=sc.nextInt();
+        if(obj1.containsElement(searchElement))
+            System.out.println("Array contains element");
+        else
+            System.out.println("Array does not contain element");
+      System.out.println("enter index at which element needs to be deleted");
+      int deleteIndex=sc.nextInt();
+      System.out.println("status of deletion: "+ obj1.deleteElement(deleteIndex));
     }
 }
